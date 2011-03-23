ddb92998a158d1d5800d64af74aaaee4d0ac38cb
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-03-23 17:18:06 -0400

    Switching back to Firefox, with direct driver, using new Selenium.

--- regscrape/settings.py
+++ regscrape/settings.py
@@ -1,7 +1,7 @@
 TARGET_SERVER = 'www.regulations.gov'
 PER_PAGE = 50
-INSTANCES = 2
+INSTANCES = 4
 CLEAR_FIRST = True
 MAX_RECORDS = 10000
 # BROWSER = {'driver': 'Remote', 'kwargs': {'browser_name': 'Firefox'}}
-BROWSER = {'driver': 'Chrome'}
+BROWSER = {'driver': 'Firefox'}
