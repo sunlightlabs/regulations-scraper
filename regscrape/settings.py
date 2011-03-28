980074d1e76d7107a3206c5e9aa572cec73d5285
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-03-28 17:21:15 +0000

    Adding local settings, per the Django idiom.

--- regscrape/settings.py
+++ regscrape/settings.py
@@ -1,8 +1,13 @@
 TARGET_SERVER = 'www.regulations.gov'
 PER_PAGE = 50
-INSTANCES = 4
+INSTANCES = 6
 CLEAR_FIRST = True
-MAX_RECORDS = 10000
+MAX_RECORDS = 0
 SEARCH = {'dct': 'PS'}
 # BROWSER = {'driver': 'Remote', 'kwargs': {'browser_name': 'Firefox'}}
 BROWSER = {'driver': 'Firefox'}
+
+try:
+    from local_settings import *
+except:
+    pass
