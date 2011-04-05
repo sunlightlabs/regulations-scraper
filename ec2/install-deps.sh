4763ffa0fa7911d91ccb58725b47a9367f4ea2f6
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-04-05 20:32:34 +0000

    Adding openbox and tesseract 3 to install script.

--- ec2/install-deps.sh
+++ ec2/install-deps.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 apt-get update
-apt-get install -y xvfb libgtk2.0-0 libasound2 build-essential python-dev libevent-dev mongodb git python-pip python-virtualenv virtualenvwrapper puf
+apt-get install -y xvfb libgtk2.0-0 libasound2 build-essential python-dev libevent-dev mongodb git python-pip python-virtualenv virtualenvwrapper puf openbox
 
 cd /opt
 wget http://releases.mozilla.org/pub/mozilla.org/firefox/releases/4.0/linux-x86_64/en-US/firefox-4.0.tar.bz2 -O - | tar xvfj -
 ln -s /opt/firefox/firefox /usr/bin/firefox
+
+mkdir /tmp/tesseract
+cd /tmp/tesseract
+wget http://ppa.launchpad.net/alex-p/notesalexp/ubuntu/pool/main/t/tesseract/tesseract-ocr_3.0.0+svn581-1ppa1~maverick1_i386.deb
+wget http://ppa.launchpad.net/alex-p/notesalexp/ubuntu/pool/main/t/tesseract/tesseract-ocr-eng_3.0.0+svn581-1ppa1~maverick1_all.deb
+dpkg -i *.deb
+cd
+rm -rf /tmp/tesseract
+apt-get install -f
