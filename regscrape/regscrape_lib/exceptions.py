8bdb7308921812ed7c1c774f769a0a2b1b844836
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-06-17 12:47:53 -0400

    Ripping out all of the selenium stuff.  Sad face.

--- regscrape/regscrape_lib/exceptions.py
+++ regscrape/regscrape_lib/exceptions.py
@@ -1,12 +1,3 @@
-class StillNotFound(Exception):
-    pass
-
-class Finished(Exception):
-    pass
-
-class FoundErrorElement(Exception):
-    pass
-
 class DecodeFailed(Exception):
     pass
 
--- regscrape/settings.py
+++ regscrape/settings.py
@@ -1,27 +1,15 @@
 TARGET_SERVER = 'www.regulations.gov'
-INSTANCES = 6
-#BROWSER = {'driver': 'Remote', 'kwargs': {'browser_name': 'Firefox'}}
-BROWSER = {'driver': 'Firefox'}
 DEBUG = True
 DB_NAME = 'regulations'
 DATA_DIR = '/data'
 DECODERS = 2
 
-# settings for bulk API dumping (mode 'prepopulate')
 DUMP_START = 0
 DUMP_END = 3500000
 DUMP_INCREMENT = 100000
 MAX_WAIT = 600
 CHUNK_SIZE = 10
 FILTER = {}
-
-# settings for browser-based search (mode 'search')
-SEARCH = {'dct': 'PS'}
-PER_PAGE = 50
-MAX_RECORDS = 0
-#MAX_WAIT = 600
-
-MODE = 'prepopulate'
 
 try:
     from local_settings import *
