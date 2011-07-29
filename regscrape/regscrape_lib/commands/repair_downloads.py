207367c713503b860d033bda205b5a7928f5e28f
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-07-29 21:22:53 +0000

    Quick spot-checking command to make sure all the files are there that are supposed to be.

---
+++ regscrape/regscrape_lib/commands/repair_downloads.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from regscrape_lib.processing import *
+from regscrape_lib.util import download, download_wget
+import settings
+import subprocess, os, urlparse, sys, traceback, datetime
+from gevent.pool import Pool
+import urllib2
+import pymongo
+
+MIN_SIZE = getattr(settings, 'MIN_DOWNLOAD_SIZE', 1024)
+
+def run():
+    run_for_view_type('document views', find_views, update_view)
+    run_for_view_type('attachment views', find_attachment_views, update_attachment_view)
+    
+def run_for_view_type(view_label, find_func, update_func):
+    print 'Checking download of %s.' % view_label
+    
+    views = find_func(downloaded=True, query=settings.FILTER)
+    
+    # keep the decoders busy with tasks as long as there are more results
+    for result in views:
+        state = None
+        error = False
+        try:
+            stat = os.stat(result['view']['file'])
+            if stat.st_size < MIN_SIZE:
+                print "Oh noes, file %s of the %s of %s is too small." % (result['view']['file'], view_label, result['doc'])
+                error = True
+        except OSError:
+            print "Oh noes, file %s of the %s of %s isn't there." % (result['view']['file'], view_label, result['doc'])
+            error = True
+        
+        if error:
+            result['view']['downloaded'] = False
+            update_func(**result)
+            if stat:
+                os.unlink(result['view']['file'])
+    
+    print 'Done with %s.' % view_label
+
+if __name__ == "__main__":
+    run()
