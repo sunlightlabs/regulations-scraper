07a147bd8a819195b6cdf11c16aaf4b1a2112b97
Andrew Pendleton <apendleton@sunlightfoundation.com>
2011-10-13 20:59:40 +0000

    Preliminary stats tracking for dumper.

--- regscrape/regscrape_lib/commands/dump_api.py
+++ regscrape/regscrape_lib/commands/dump_api.py
@@ -9,6 +9,9 @@ def run():
     # delete old dumps
     [os.unlink(os.path.join(settings.DUMP_DIR, file)) for file in os.listdir(settings.DUMP_DIR) if file.endswith('.gwt')]
     
+    # keep stats
+    stats = {'downloaded': 0, 'failed': 0}
+    
     # start new dumps
     client = RegsClient()
     position = settings.DUMP_START
@@ -21,6 +24,7 @@ def run():
                     search(settings.DUMP_INCREMENT, position, client),
                     os.path.join(settings.DUMP_DIR, 'dump_%s.gwt' % str(position).zfill(num_digits)),
                 )
+                stats['downloaded'] += 1
                 break
             except urllib2.HTTPError:
                 if i < 2:
@@ -28,6 +32,9 @@ def run():
                     time.sleep(10)
                 else:
                     print 'System troubles; giving up.'
-                    sys.exit(1)
+                    stats['failed'] += 1
+                    return stats
         
         position += settings.DUMP_INCREMENT
+    
+    return stats
