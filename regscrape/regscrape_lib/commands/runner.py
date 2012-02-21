c35be7909ba8310140cea03fddaa71f062a2aefe
Andrew Pendleton <apendleton@sunlightfoundation.com>
2012-02-21 14:23:53 -0500

    Add support for running one-off scripts via the regscrape infrastructure.

--- regscrape/regscrape_lib/commands/runner.py
+++ regscrape/regscrape_lib/commands/runner.py
@@ -8,12 +8,21 @@ def run_command():
         sys.exit()
     command = sys.argv[1]
     
-    try:
-        parent_mod = __import__('regscrape_lib.commands', fromlist=[command])
-        mod = getattr(parent_mod, command)
-    except ImportError:
-        print 'No such command: %s' % command
-        sys.exit()
+    if command.endswith('.py'):
+        mod_name = command.split('/').pop().rsplit('.', 1)[0]
+        import imp
+        try:
+            mod = imp.load_source(mod_name, command)
+        except ImportError:
+            print 'Could not load custom command: %s' % command
+            sys.exit()
+    else:
+        try:
+            parent_mod = __import__('regscrape_lib.commands', fromlist=[command])
+            mod = getattr(parent_mod, command)
+        except ImportError:
+            print 'No such command: %s' % command
+            sys.exit()
     
     if getattr(mod, 'GEVENT', True):
         from gevent.monkey import patch_all
@@ -32,9 +41,9 @@ def run_command():
     parser.add_option('--parsable', dest='parsable', action='store_true', default=False, help='Output JSON instead of human-readable messages.')
     parse_results = parser.parse_args(sys.argv[2:])
     
+    dev_null = open('/dev/null', 'w')
     if parse_results[0].parsable:
         # disable standard output by monkey-patching sys.stdout
-        dev_null = open('/dev/null', 'w')
         real_stdout = sys.stdout
         sys.stdout = dev_null
     
@@ -46,8 +55,9 @@ def run_command():
     if parse_results[0].parsable:
         # turn stdout back on so we can print output
         sys.stdout = real_stdout
-        # but disable stderr so we can avoid that stupid gevent thing
-        sys.stderr = dev_null
         
         if out:
             print json.dumps(out)
+    
+    # no matter what, nuke stderr on exit so we can avoid that stupid gevent thing
+    sys.stderr = dev_null
