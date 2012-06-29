604001df3acbf92308b0365bf2d25798ed19bb4d
Andrew Pendleton <apendleton@sunlightfoundation.com>
2012-06-29 17:25:34 -0400

    Add database connection code.

--- regscrape/models/__init__.py
+++ regscrape/models/__init__.py
@@ -2,3 +2,7 @@ from doc import Doc, Attachment, View
 from docket import Docket
 from agency import Agency
 from entity import Entity
+
+from mongoengine import connect
+from settings import DB_NAME, DB_SETTINGS
+connect(DB_NAME, **DB_SETTINGS)
