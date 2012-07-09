83ff56009e3343844a5f7f3c76edfc553036ba42
Andrew Pendleton <apendleton@sunlightfoundation.com>
2012-07-09 17:42:24 -0400

    Refactoring of the dump code to make docket and agency filters work.

--- regscrape/regsdotgov/commands/rdg_dump_api.py
+++ regscrape/regsdotgov/commands/rdg_dump_api.py
@@ -1,28 +1,47 @@
-def run():
+from optparse import OptionParser
+arg_parser = OptionParser()
+arg_parser.add_option("-a", "--agency", dest="agency", action="store", type="string", default=None, help="Specify an agency to which to limit the dump.")
+arg_parser.add_option("-d", "--docket", dest="docket", action="store", type="string", default=None, help="Specify a docket to which to limit the dump.")
+
+def run(options, args):
     import urllib2
     import settings
     import os, time, sys
     from regsdotgov.regs_gwt.regs_client import RegsClient
     from regsdotgov.search import search
     from regs_common.transfer import download
-    
+
+    search_args = {}
+    id_string = 'all'
+    if options.agency and options.docket:
+        raise Exception("Specify either an agency or a docket")
+    elif options.agency:
+        search_args = {'agency': options.agency}
+        id_string = 'agency_' + options.agency
+    elif options.docket:
+        search_args = {'docket': docket}
+        id_string = 'docket_' + options.docket.replace('-', '_')
+
     # delete old dumps
-    [os.unlink(os.path.join(settings.DUMP_DIR, file)) for file in os.listdir(settings.DUMP_DIR) if file.endswith('.gwt')]
+    [os.unlink(os.path.join(settings.DUMP_DIR, file)) for file in os.listdir(settings.DUMP_DIR) if file.startswith('dump_%s' % id_string) and file.endswith('.gwt')]
     
     # keep stats
     stats = {'downloaded': 0, 'failed': 0}
     
     # start new dumps
     client = RegsClient()
-    position = settings.DUMP_START
+    position = 0
+    total = 1
     num_digits = len(str(settings.DUMP_END))
-    while position <= settings.DUMP_END:
+    while position <= total:
         for i in range(3):
             try:
-                print "Downloading page %s of %s..." % ((position / settings.DUMP_INCREMENT) + 1, ((settings.DUMP_END - settings.DUMP_START) / settings.DUMP_INCREMENT) + 1)
-                download(
-                    search(settings.DUMP_INCREMENT, position, client),
-                    os.path.join(settings.DUMP_DIR, 'dump_%s.gwt' % str(position).zfill(num_digits)),
+                current_str = (position / settings.DUMP_INCREMENT) + 1
+                total_str = '?' if total == 1 else (total / settings.DUMP_INCREMENT) + 1
+                print "Downloading page %s of %s..." % (current_str, total_str)
+                total = download(
+                    search(settings.DUMP_INCREMENT, position, client, **search_args),
+                    os.path.join(settings.DUMP_DIR, 'dump_%s_%s.gwt' % (id_string, str(position).zfill(num_digits))),
                 )
                 stats['downloaded'] += 1
                 break
--- regscrape/regsdotgov/search.py
+++ regscrape/regsdotgov/search.py
@@ -2,11 +2,16 @@ import urllib2
 from pygwt.response import Response
 
 REQUEST_DATA = "7|0|14|http://www.regulations.gov/Regs/|C006AEC3A690AD65DC608DB5A8DBA002|com.gwtplatform.dispatch.shared.DispatchService|execute|java.lang.String/2004016611|com.gwtplatform.dispatch.shared.Action|6dfecc389e4d86b4e63e6b459bfa29e673a88555d93b469fc5ed362134c31079.e38Sb3aKaN8Oe3uRai0|gov.egov.erule.regs.shared.action.LoadSearchResultsAction/125242584|gov.egov.erule.regs.shared.models.SearchQueryModel/2202288755|java.util.ArrayList/4159755760||gov.egov.erule.regs.shared.models.DataFetchSettings/3659460086|java.lang.Integer/3438268394|java.lang.Boolean/476441737|1|2|3|4|2|5|6|7|8|0|9|0|10|0|11|11|11|11|10|0|3|10|0|12|13|%s|13|%s|11|11|14|1|1|0|"
+FILTERED_REQUEST_DATA = {
+    'agency': "7|0|16|http://www.regulations.gov/Regs/|C006AEC3A690AD65DC608DB5A8DBA002|com.gwtplatform.dispatch.shared.DispatchService|execute|java.lang.String/2004016611|com.gwtplatform.dispatch.shared.Action|afc0dc8034150b895be382fee05843c5149cfa1f773f9885aa6d180fd7f2d5af.e38Sc3uTa3qQe34Qa350|gov.egov.erule.regs.shared.action.LoadSearchResultsAction/125242584|gov.egov.erule.regs.shared.models.SearchQueryModel/2202288755|java.util.ArrayList/4159755760||gov.egov.erule.regs.shared.models.Agency/3251642055|{agency}|java.lang.Integer/3438268394|gov.egov.erule.regs.shared.models.DataFetchSettings/3659460086|java.lang.Boolean/476441737|1|2|3|4|2|5|6|7|8|0|9|0|10|0|11|11|11|11|10|1|12|13|14|0|11|11|3|10|0|15|14|%s|14|%s|11|11|16|1|1|0|",
+    'docket': "7|0|16|http://www.regulations.gov/Regs/|C006AEC3A690AD65DC608DB5A8DBA002|com.gwtplatform.dispatch.shared.DispatchService|execute|java.lang.String/2004016611|com.gwtplatform.dispatch.shared.Action|afc0dc8034150b895be382fee05843c5149cfa1f773f9885aa6d180fd7f2d5af.e38Sc3uTa3qQe34Qa350|gov.egov.erule.regs.shared.action.LoadSearchResultsAction/125242584|gov.egov.erule.regs.shared.models.SearchQueryModel/2202288755|java.util.ArrayList/4159755760|{docket}||gov.egov.erule.regs.shared.models.DocumentType/2460330259|gov.egov.erule.regs.shared.models.DataFetchSettings/3659460086|java.lang.Integer/3438268394|java.lang.Boolean/476441737|1|2|3|4|2|5|6|7|8|1|9|0|10|0|11|12|12|12|10|5|13|4|13|5|13|3|13|1|13|2|3|10|0|14|15|%s|15|%s|12|12|16|1|1|0|"
+}
 
-def search(per_page, position, client):
+def search(per_page, position, client, **args):
+    request_data = FILTERED_REQUEST_DATA[args.keys()[0]].format(**args) if args else REQUEST_DATA
     return urllib2.urlopen(urllib2.Request(
         'http://www.regulations.gov/dispatch/LoadSearchResultsAction',
-        REQUEST_DATA % (per_page, position),
+        request_data % (per_page, position),
         client.headers
     ))
 
@@ -17,12 +22,12 @@ def parse(file, client):
     return response.reader.read_object()
 
 # convenience function that strings them together
-def parsed_search(per_page, position, client=None):
+def parsed_search(per_page, position, client=None, **args):
     if not client:
         from regs_gwt.regs_client import RegsClient
         client = RegsClient()
-    
-    return parse(search(per_page, position, client), client)
+
+    return parse(search(per_page, position, client, **args), client)
 
 # use the search with an overridden client to get the agencies instead of the documents
 def get_agencies():
