98f44513b61fb12f67c4ba32ca05551212b0d0d5
Andrew Pendleton <apendleton@sunlightfoundation.com>
2013-08-29 15:21:45 -0400

    Add ability to update existing entity lists preserving stats.

--- regscrape/regs_common/commands/create_entities.py
+++ regscrape/regs_common/commands/create_entities.py
@@ -2,8 +2,20 @@ GEVENT = False
 
 from regs_models import *
 
-def run():
+from optparse import OptionParser
+arg_parser = OptionParser()
+arg_parser.add_option("-U", "--update", dest="update", action="store_true", default=False, help="Check if entities already existing before creating (slower)")
+
+def run(options, args):
     import settings
+
+    # if we're updating
+    if options.update:
+        print "Preparing to update existing entities; retrieving current entity list..."
+        current = set((e.id for e in Entity.objects()))
+        print "Entities retrieved."
+    else:
+        print "Constructing new entity list."
 
     # grab a dictionary
     word_file = getattr(settings, 'WORD_FILE', '/usr/share/dict/words')
@@ -27,8 +39,12 @@ def run():
             'aliases': normalize_list([entity['name']] + entity['aliases'], entity['type'])
         }
         record['filtered_aliases'] = [alias for alias in record['aliases'] if alias.lower() not in english_words]
-        
-        db_entity = Entity(**record)
-        db_entity.save()
-
-        print "Saved %s as %s" % (record['aliases'][0], record['id'])
+                
+        if options.update and record['id'] in current:
+            Entity.objects(id=record['id']) \
+                  .update(safe_update=True, set__td_type=record['td_type'], set__td_name=record['td_name'], set__aliases=record['aliases'], set__filtered_aliases=record['filtered_aliases'])
+            print "Updated %s as existing record %s" % (record['aliases'][0], record['id'])
+        else:
+            db_entity = Entity(**record)
+            db_entity.save()
+            print "Saved %s as new record %s" % (record['aliases'][0], record['id'])
