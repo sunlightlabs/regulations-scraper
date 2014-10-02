fa091fc7f0cb31cf0300119b8a0b42fa63e71448
Andrew Pendleton <apendleton@sunlightfoundation.com>
2014-10-02 02:53:57 -0400

    Add command to reset extraction failures.

---
+++ regscrape/regs_common/commands/reset_extraction.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+def run():
+    global os, settings
+    from regs_common.processing import find_views, update_view, find_attachment_views, update_attachment_view
+    import os
+    import settings
+
+    run_for_view_type('document views', find_views, update_view)
+    run_for_view_type('attachment views', find_attachment_views, update_attachment_view)
+
+def run_for_view_type(view_label, find_func, update_func):
+    print 'Resetting %s.' % view_label
+    views = find_func(extracted='failed', query={'deleted': False})
+    
+    for result in views:
+        result['view'].extracted = 'no'
+        update_func(**result)
+    
+    print 'Done with %s.' % view_label
+
+if __name__ == "__main__":
+    run()
